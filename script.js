a1b26ea7380efc8debdbce891c0acbe486a41d7b
fazrienoer7-sys <fazrienoer7@gmail.com>
2026-05-20 16:40:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* NAVBAR EFFECT */
+/* NAVBAR */
 
 window.addEventListener("scroll", function(){
 
@@ -142,7 +142,7 @@ document.getElementById("bg-music");
 const musicBtn =
 document.getElementById("music-toggle");
 
-music.volume = 0.20;
+music.volume = 0.35;
 
 musicBtn.addEventListener("click", () => {
 
@@ -199,37 +199,6 @@ window.addEventListener(
 );
 
 revealSections();
-
-/* PARTNERSHIP HOVER EFFECT */
-
-const partnerCards =
-document.querySelectorAll(
-  ".partner-card"
-);
-
-partnerCards.forEach(card => {
-
-  card.addEventListener(
-    "mouseenter",
-    () => {
-
-      card.style.transform =
-      "translateY(-8px) scale(1.03)";
-
-    }
-  );
-
-  card.addEventListener(
-    "mouseleave",
-    () => {
-
-      card.style.transform =
-      "translateY(0px) scale(1)";
-
-    }
-  );
-
-});
 
 console.log(
   "Unity~LegiOn` Website Ready!"
